0dbe8a9baddf856d5bb8be34bbfdde64ef8a4000
Deivson <dvs.santos@gmail.com>
2016-06-25 11:53:49 -0300

    Segundo commit - Add a interface Control

---
+++ control/EspecialidadeControl.php
@@ -0,0 +1,24 @@
+<?php
+
+class EspecialidadeControl {
+    
+    public function inserir(){
+        
+    }
+    public function listarTodos(){
+        
+        
+    }
+    public function buscar(){
+        
+        
+    }
+    public function editar(){
+        
+        
+    }
+    public function deletar(){
+        
+        
+    }
+}
