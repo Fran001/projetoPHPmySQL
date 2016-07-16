6153bb27ffa231bbddf4625f2d990e67de149b3d
Deivson <dvs.santos@gmail.com>
2016-07-16 12:37:06 -0300

    Menu Bar - Estilização da tela listar especialidades

--- view/listarEspecialidades.php
+++ view/listarEspecialidades.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once '../style/template.php';
 include_once '../control/EspecialidadeControl.php';
 $especialidadeControl = new EspecialidadeControl()
        
@@ -7,12 +7,16 @@ $especialidadeControl = new EspecialidadeControl()
 //print_r($especialidadeControl->listarTodos());//usado para debugar o código;
 
 //LISTAR OBJETOS
-?> 
-
-<table>
+?>
+<div class="col-sm-10 col-sm-offset-1">
+<h1>Especialidades</h1>
+<a href="" class="pull-right btn-primary btn-xs">Cadastrar Especialidades</a>
+<table class="jumbotron table table-striped">
     <tr>
         <th>id</th>
         <th>Nome</th>
+    <!--Aumentando o tamanho da coluna peincipal para receber listar/deletar-->
+        <th></th>  
        
     </tr>
     <?php
@@ -22,7 +26,18 @@ $especialidadeControl = new EspecialidadeControl()
     <tr>
         <td><?php echo $especialidade->id;?></td>
         <td><?php echo $especialidade->nome;?></td>
-       
+        <!--Adicionando os campos editar|deletar-->
+        <td>
+            <div class="pull-right">
+                <a href="">Editar</a>  
+                | 
+                <a href="">Deletar</a>
+            </div>
+        </td>
     </tr>
+    
     <?php } ?>
 </table>
+</div>
+<?php include_once '../style/footer.php';
+ 
